5afb782d841b4ee51229a5cbc5a103ba3b898d9d
王剑锋 <wangjianfeng@wangjianfengdeMacBook-Pro.local>
2021-10-18 20:30:34 +0800

    first

---
+++ c_learning/src/hello.c
@@ -0,0 +1,10 @@
+//
+// Created by 王剑锋 on 2021/9/1.
+//
+# include <stdio.h>
+
+int main(){
+    // 测试
+    printf("hello world ! \n");
+    return 0;
+}
---
+++ c_learning/src/plus.c
@@ -0,0 +1,14 @@
+//
+// Created by 王剑锋 on 2021/9/1.
+//
+
+#include <stdio.h>
+
+int main(){
+    int price=100;
+    float give;
+    scanf("%f", &give);
+    printf("out of price %.2f \n", give/price);
+}
+
+
