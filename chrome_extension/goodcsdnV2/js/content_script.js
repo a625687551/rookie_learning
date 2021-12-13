39473be15983cd5bc0d6cdf741b44c0cdcad562c
wangjianfeng1 <wangjianfeng1@360.cn>
2021-12-13 21:56:54 +0800

    add attention for watch

--- chrome_extension/goodcsdnV2/js/content_script.js
+++ chrome_extension/goodcsdnV2/js/content_script.js
@@ -1,17 +1,6 @@
 // 所谓content-scripts，其实就是Chrome插件中向页面注入脚本的一种形式（虽然名为script，其实还可以包括css的），
 // 借助content-scripts我们可以实现通过配置的方式轻松向指定页面注入JS和CSS（如果需要动态注入，可以参考下文），
 // 最常见的比如：广告屏蔽、页面CSS定制，等等。
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function(detail) {
-//       console.log("fuuck,...")
-//       return {cancel : true};
-//       },
-//   {urls:[
-//       "*://g.csdnimg.cn/common/csdn-login-box/*"
-//     ]},
-//   extraInfoSpec = ["blocking"] //类型blocking为拦截,
-//
-// );
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   switch (request.type) {
@@ -19,9 +8,17 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       sendResponse(document.body.contentEditable);
       break;
     case 'toggle':
+      // 打开复制
       document.body.contentEditable = ![true, 'true'].includes(
         document.body.contentEditable
       );
+      // 移除关注可见
+      document.getElementById("article_content").removeAttribute("style");
+      // 移除关注可见的标签。
+      let attention_tag = document.getElementsByClassName("hide-article-box");
+      if(attention_tag.length >0){
+        attention_tag[0].remove();
+      }
     default:
       break;
   }
--- chrome_extension/goodcsdnV2/js/popup.js
+++ chrome_extension/goodcsdnV2/js/popup.js
@@ -21,3 +21,5 @@ function sendMessageToContentScript(message, callback) {
   });
 }
 
+
+
